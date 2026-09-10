1469a5e47e4485f599f4deef9b28a0c743c1c4c7
vaishnavi112007 <vaishusmohite21@gmail.com>
2026-09-10 15:30:08 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,11 @@
+
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+address = input("Enter Address: ")
+prn = input("Enter PRN: ")
+
+print("\n--- Student Details ---")
+print("Name    :", name)
+print("Age     :", age)
+print("Address :", address)
+print("PRN     :", prn)
