f4d9a5279db2733da8bc4953abcc6f793790338c
vaishnavi112007 <vaishusmohite21@gmail.com>
2026-09-10 15:35:29 +0530

    Add files via upload

---
+++ lab9.2.py
@@ -0,0 +1,11 @@
+grades = [75, 82, 68, 90, 77]
+
+print("Original Grades:", grades)
+
+index = int(input("Enter the index position to update: "))
+
+new_grade = int(input("Enter the new grade: "))
+
+grades[index] = new_grade;
+
+print("Corrected Grades:", grades)
