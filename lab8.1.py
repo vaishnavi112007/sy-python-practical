910d4b11c58324494fdae8d69c13aca365a9c7c7
vaishnavi112007 <vaishusmohite21@gmail.com>
2026-09-10 15:33:37 +0530

    Add files via upload

---
+++ lab8.1.py
@@ -0,0 +1,9 @@
+
+
+first_name = input("Enter your first name: ").strip()
+last_name = input("Enter your last name: ").strip()
+
+
+full_name = f"{first_name.title()} {last_name.title()}"
+
+print("Clean Full Name:", full_name)
