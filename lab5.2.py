1701ed2c9bd91a992ce3a1117d70c84ded35eea3
vaishnavi112007 <vaishusmohite21@gmail.com>
2026-09-10 15:31:21 +0530

    Add files via upload

---
+++ lab5.2.py
@@ -0,0 +1,11 @@
+
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+
+for i in range(rows):
+    for j in range(columns):
+        if i == 0 or i == rows - 1 or j == 0 or j == columns - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
